1138be0f648650c3c34ad983dc93b05f967c9402
Domenico Tenace <domenicotenace99@gmail.com>
2023-04-07 17:27:22 +0200

    feat: create button component

---
+++ src/components/button/Button.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+type ButtonType = "button" | "submit" | "reset";
+
+interface Button {
+  type?: ButtonType;
+  disabled?: boolean;
+  formId?: string;
+  autoFocus?: boolean;
+  customClasses?: string[] | object;
+}
+const props = defineProps<Button>();
+</script>
+<template>
+  <button
+    :type="props.type ?? 'button'"
+    :class="customClasses ?? 'button'"
+    :disabled="props.disabled ?? false"
+    :autofocus="props.autoFocus ?? false"
+    :form="props.formId ?? undefined"
+  >
+    <slot></slot>
+  </button>
+</template>

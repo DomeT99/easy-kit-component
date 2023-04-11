13fd76b7cf3684dcae9cbd0973505089e383aa57
Domenico Tenace <domenicotenace99@gmail.com>
2023-04-11 12:22:19 +0200

    feat: styling all button

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import Button from "./components/button/Button.vue";
-
 </script>
 <template>
   <Button>Ciao</Button>
+  <Button :custom-classes="['button', 'is-secondary']">Ciao</Button>
+  <Button :custom-classes="['button', 'is-tertiary']">Ciao</Button>
 </template>

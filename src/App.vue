72b46a3264946f5b2969dbc2742daab40f1a640c
Domenico Tenace <domenicotenace99@gmail.com>
2023-04-11 12:38:30 +0200

    feat: setup test

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,2 @@
-<script setup lang="ts">
-import Button from "./components/button/Button.vue";
-</script>
-<template>
-  <Button>Ciao</Button>
-  <Button :custom-classes="['button', 'is-secondary']">Ciao</Button>
-  <Button :custom-classes="['button', 'is-tertiary']">Ciao</Button>
-</template>
+<script setup lang="ts"></script>
+<template></template>

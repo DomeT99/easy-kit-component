4fbd6e52bd3c04d9c77f071a9f430cb816c56d6e
Domenico Tenace <domenicotenace99@gmail.com>
2023-04-13 14:23:53 +0200

    setup: test index html

--- src/components/button/Button.test.ts
+++ src/components/button/Button.test.ts
@@ -4,31 +4,34 @@ import { describe, expect, it, vi } from "vitest";
 
 let wrapper;
 
-describe.skip("EButton suite", () => {
+describe("EButton suite", () => {
   it("render correctly", () => {
     wrapper = mount(EButton);
 
     expect(wrapper.html()).toMatchSnapshot();
   });
 
-  it("render default classes", () => {
-    wrapper = mount(EButton, {
+  it("render default class", () => {
+    const options = {
       props: {
         customClasses: undefined,
       },
-    });
+    };
+
+    wrapper = mount(EButton, options);
 
     expect(wrapper.classes()).toEqual(["e-button"]);
   });
 
   it("trigger click event", () => {
     const click = vi.fn();
-
-    wrapper = mount(EButton, {
+    const options = {
       listeners: {
         click: click(),
       },
-    });
+    };
+
+    wrapper = mount(EButton, options);
     wrapper.find("button").trigger("click");
 
     expect(click).toHaveBeenCalledTimes(1);
--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-
-//CSS
-import "./scss/global.scss";
-
-createApp(App).mount("#app");

027d52783373ed335d0a26a66124d76f92b20b52
Domenico Tenace <domenicotenace99@gmail.com>
2023-04-20 17:45:42 +0200

    test: test about placeholder select component

--- src/components/select/Select.test.ts
+++ src/components/select/Select.test.ts
@@ -7,31 +7,36 @@ let wrapper;
 describe("ESelect suite", () => {
   it("render correctly", () => {
     wrapper = mount(ESelect);
-
     expect(wrapper.html()).toMatchSnapshot();
   });
 
   it("render default class", () => {
     wrapper = mount(ESelect);
-
     expect(wrapper.classes()).toEqual(["e-select"]);
   });
 
   it("render default multiple", () => {
     wrapper = mount(ESelect);
-
     expect(wrapper.attributes("multiple")).toBe(undefined);
   });
-  
+
   it("render default required", () => {
     wrapper = mount(ESelect);
-
     expect(wrapper.attributes("required")).toBe(undefined);
   });
-  
+
   it("render default disabled", () => {
     wrapper = mount(ESelect);
-
     expect(wrapper.attributes("disabled")).toBe(undefined);
+  });
+
+  it("render correctly placeholder", () => {
+    const options = {
+      props: {
+        placeHolder: "Test",
+      },
+    };
+    wrapper = mount(ESelect, options);
+    expect(wrapper.vm.placeHolder).toBe("Test");
   });
 });

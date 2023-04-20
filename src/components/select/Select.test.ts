56058b9d9bcce008abb261584a8b3cd07682cb03
Domenico Tenace <domenicotenace99@gmail.com>
2023-04-20 15:09:32 +0200

    test: write all tests of routine

--- src/components/select/Select.test.ts
+++ src/components/select/Select.test.ts
@@ -5,9 +5,33 @@ import { describe, expect, it, vi } from "vitest";
 let wrapper;
 
 describe("ESelect suite", () => {
-  it.only("render correctly", () => {
+  it("render correctly", () => {
     wrapper = mount(ESelect);
 
     expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it("render default class", () => {
+    wrapper = mount(ESelect);
+
+    expect(wrapper.classes()).toEqual(["e-select"]);
+  });
+
+  it("render default multiple", () => {
+    wrapper = mount(ESelect);
+
+    expect(wrapper.attributes("multiple")).toBe(undefined);
+  });
+  
+  it("render default required", () => {
+    wrapper = mount(ESelect);
+
+    expect(wrapper.attributes("required")).toBe(undefined);
+  });
+  
+  it("render default disabled", () => {
+    wrapper = mount(ESelect);
+
+    expect(wrapper.attributes("disabled")).toBe(undefined);
   });
 });

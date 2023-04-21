0f27817690e1ae9678e57ef85d62f90747f2212a
Dvalin <domenicotenace99@gmail.com>
2023-04-21 19:16:34 +0200

    test: last test of radio component

--- src/components/radio/Radio.test.ts
+++ src/components/radio/Radio.test.ts
@@ -1,5 +1,5 @@
 import { mount, shallowMount } from "@vue/test-utils";
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import ERadio from "./Radio.vue";
 
 describe("ERadio suite", () => {
@@ -22,5 +22,19 @@ describe("ERadio suite", () => {
     wrapper = mount(ERadio, options);
 
     expect(wrapper.props("name")).toEqual("test");
+  });
+
+  it.only("trigger click event", () => {
+    const change = vi.fn(() => true);
+    const options = {
+      listeners: {
+        change: change(),
+      },
+    };
+
+    wrapper = mount(ERadio, options);
+    wrapper.find("input").trigger("change");
+
+    expect(change).toHaveReturnedWith(true);
   });
 });

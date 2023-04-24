314dd577dca57e3ef0125edd9a85cd2a5d39e8e6
Domenico Tenace <domenicotenace99@gmail.com>
2023-04-24 13:35:48 +0200

    set: setup for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,34 @@
 // @ts-nocheck
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  test:{
-    globals:true,
-    environment: 'happy-dom',
-  }
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "./src/index.ts"),
+      name: "EasyKit",
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: "happy-dom",
+  },
+});

04703701deccb96ca2d2f2b4da47a8d4f3436cc6
Domenico Tenace <domenicotenace99@gmail.com>
2023-04-27 11:06:09 +0200

    docs: active docs button

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,40 +1,44 @@
-import { defineConfig } from 'vitepress'
-
+import { defineConfig } from "vitepress";
 
 export default defineConfig({
   title: "Easy Component Kit ",
   description: "Simple components kit for Vue 3!",
   themeConfig: {
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Docs', link: '../getting-started/quick-start.md' }
+      { text: "Home", link: "/" },
+      {
+        text: "Docs",
+        link: "../getting-started/quick-start.md",
+        activeMatch: "/getting-started/",
+      },
     ],
 
     sidebar: [
       {
-        text: 'Getting Started',
+        text: "Getting Started",
         items: [
-          { text: 'Quick Start', link: '../getting-started/quick-start.md' },
-        ]
+          { text: "Quick Start", link: "../getting-started/quick-start.md" },
+        ],
       },
       {
-        text: 'Components',
+        text: "Components",
         items: [
-          { text: 'EButton', link: '../components/ebutton.md' },
-          { text: 'ECheckbox', link: '../components/echeckbox.md' },
-          { text: 'EText', link: '../components/etext.md' },
-          { text: 'ERadio', link: '../components/eradio.md' },
-          { text: 'ESelect', link: '../components/eselect.md' },
-        ]
-      }
+          { text: "EButton", link: "../components/ebutton.md" },
+          { text: "ECheckbox", link: "../components/echeckbox.md" },
+          { text: "EText", link: "../components/etext.md" },
+          { text: "ERadio", link: "../components/eradio.md" },
+          { text: "ESelect", link: "../components/eselect.md" },
+        ],
+        collapsed: true,
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/DomeT99/easy-kit-component' }
+      { icon: "github", link: "https://github.com/DomeT99/easy-kit-component" },
     ],
     footer: {
-      message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2022-present Domenico Tenace'
-    }
-  }
-})
+      message: "Released under the MIT License.",
+      copyright: "Copyright © 2022-present Domenico Tenace",
+    },
+  },
+});
